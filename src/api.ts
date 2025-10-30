62f8a2bcbd986d6c4e3c78a8fd03b1d445eb6d01
Arthur <arthur@entrlcom.org>
2025-10-30 17:18:54 +0200

    feat: init (#1)
    * chore: init
    
    * fix: add prerelease branch
    
    * fix: update ci publish
    
    * fix: add type module
    
    * fix: update lint
    
    * fix: contents write
    
    * chore(release): 1.0.0-develop.1 [skip ci]
    
    # 1.0.0-develop.1 (2025-10-30)
    
    ### Bug Fixes
    
    * add prerelease branch ([fa2ce43](https://github.com/flida-dev/web-sdk/commit/fa2ce4313972b35aab0fc4b4fa6baa4a96396e63))
    * add type module ([ab223d5](https://github.com/flida-dev/web-sdk/commit/ab223d58b1ce665fc9e022dad0b940e96253b30a))
    * contents write ([db3a121](https://github.com/flida-dev/web-sdk/commit/db3a12178e3459584482abcf7a82a695d53f5361))
    * update ci publish ([d51bd15](https://github.com/flida-dev/web-sdk/commit/d51bd15840060226e726c69f7d4d176e1b2f9668))
    * update lint ([ed4a13f](https://github.com/flida-dev/web-sdk/commit/ed4a13fb15a68608ebb192b4f799101e88149767))
    
    * fix: exports
    
    * fix: update exclude
    
    * fix: update releaserc
    
    * fix: update releaserc
    
    * chore(release): 1.0.0-develop.2 [skip ci]
    
    # [1.0.0-develop.2](https://github.com/flida-dev/web-sdk/compare/v1.0.0-develop.1...v1.0.0-develop.2) (2025-10-30)
    
    ### Bug Fixes
    
    * exports ([ff011a8](https://github.com/flida-dev/web-sdk/commit/ff011a8d94df19fbd87171e4b15893f894e2dd75))
    * update exclude ([35b9b48](https://github.com/flida-dev/web-sdk/commit/35b9b48544d4a0228c0f130f90b39f28b57496cc))
    * update releaserc ([75d1f91](https://github.com/flida-dev/web-sdk/commit/75d1f9195d5b6dc04c1e6570bc168ee740dbe25f))
    * update releaserc ([d6042e5](https://github.com/flida-dev/web-sdk/commit/d6042e50920483db9f585dbc31a160c7ffeb09c8))
    
    * chore: remove prerelease
    
    * docs: remove changelog
    
    ---------
    
    Co-authored-by: Arthur <arthur@flida.ru>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

---
+++ src/api.ts
@@ -0,0 +1,42 @@
+import type { GetUserInfoResponse, TokenResponse } from './types'
+import { API_URL } from './constants'
+
+export class Api {
+    constructor() {
+
+    }
+
+    private async request(endpoint: string, body?: any): Promise<any> {
+        const response = await fetch(`${API_URL}/${endpoint}`, {
+            method: 'POST',
+            body: body ? JSON.stringify(body) : undefined,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+        })
+
+        const json = response.json()
+        return json
+    }
+
+    private async tokenService(endpoint: string, json: any) {
+        return this.request(`flida.token.v1.TokenService/${endpoint}`, json)
+    }
+
+    private async oidcService(endpoint: string, json: any) {
+        return this.request(`flida.oidc.v1.OIDCService/${endpoint}`, json)
+    }
+
+    public async issueToken(json: any): Promise<TokenResponse> {
+        return this.tokenService('IssueToken', json)
+    }
+
+    public async refreshToken(json: any): Promise<TokenResponse> {
+        return this.tokenService('RefreshToken', json)
+    }
+
+    public async getUserInfo(json: any): Promise<GetUserInfoResponse> {
+        return this.oidcService('GetUserInfo', json)
+    }
+}
---
+++ src/state.ts
@@ -0,0 +1,35 @@
+import { Errors, FlidaError } from './errors'
+import { clearCookie, generateId, getCookie, setCookie } from './helpers'
+
+export class State {
+    constructor(configState: string | undefined) {
+        this.state = configState || ''
+    }
+
+    private state = ''
+
+    public getState(): string {
+        if (this.state) {
+            return this.state
+        }
+
+        const oldState = getCookie('state')
+
+        if (!oldState) {
+            const newState = generateId()
+            setCookie('state', newState)
+            return newState
+        }
+
+        return oldState
+    }
+
+    public checkState(check: string): FlidaError | undefined {
+        if (this.state !== check) {
+            return new FlidaError(Errors.INVALID_STATE, 'State mismatch error')
+        }
+
+        clearCookie('state')
+        this.state = ''
+    }
+}
