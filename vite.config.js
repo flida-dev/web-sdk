9443d15c0596b144954daa83edd992ff7db30740
Arthur <arthur@flida.ru>
2025-10-30 17:51:59 +0200

    refactor: add dts

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
+  plugins: [dts(
+    {
+      entryRoot: resolve(__dirname, 'src'),
+      rollupTypes: true,
+      outputDir: 'dist',
+      copyDtsFiles: false,
+    }
+  )],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/sdk.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       formats: ['es'],
+      fileName: 'index'
     },
   },
   define: {
