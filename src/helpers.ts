abe45b1e3a74f877307e05f8eb942dc2947b89ea
Arthur <arthur@flida.ru>
2025-10-30 16:00:36 +0200

    chore: init

---
+++ src/helpers.ts
@@ -0,0 +1,55 @@
+import { nanoid } from 'nanoid/non-secure'
+
+export function generateId(size = 48) {
+    return nanoid(size)
+}
+
+export function getCookie(name: string) {
+    try {
+        const matches = document.cookie.match(new RegExp(
+            `(?:^|; )${(`flida_sdk:${name}`).replace(/([.$?*|{}()[\]\\/+^])/g, '\\$1')}=([^;]*)`,
+        ))
+        return matches ? decodeURIComponent(matches[1]) : undefined
+    }
+    // eslint-disable-next-line unused-imports/no-unused-vars
+    catch (e) {
+        return undefined
+        /* empty */
+    }
+}
+
+export function setCookie(name: string, value: string): void {
+    try {
+        const expireTime = new Date(new Date().getTime() + (100_000)).toUTCString()
+        const allowedDomain = location.host.split('.').slice(-2).join('.')
+
+        document.cookie = [
+            `flida_sdk:${name}=${encodeURIComponent(value)}`,
+            `Expires=${expireTime}`,
+            'Path=/',
+            `Domain=.${allowedDomain}`,
+            'SameSite=Strict',
+            'Secure',
+        ].join('; ')
+    }
+    // eslint-disable-next-line unused-imports/no-unused-vars
+    catch (e) {
+        /* empty */
+    }
+}
+
+export function clearCookie(name: string) {
+    const allowedDomain = location.host.split('.').slice(-2).join('.')
+    try {
+        document.cookie = [
+            `flida_sdk:${name}=`,
+            'Expires=Thu, 01 Jan 1970 00:00:00 UTC',
+            'Path=/',
+            `Domain=.${allowedDomain}`,
+            'SameSite=Strict',
+            'Secure',
+        ].join('; ')
+    }
+    // eslint-disable-next-line unused-imports/no-unused-vars
+    catch (e) {}
+}
